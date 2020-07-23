e27a9b07909b1ace7374093a24d43f73169187c6
muriloignaccio <murilohsignacio@gmail.com>
2020-07-22 23:30:08 -0300

    Implement page functionality

---
+++ public/script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('.container');
+
+const [
+  redColorSlider,
+  greenColorSlider,
+  blueColorSlider,
+] = document.querySelectorAll('input[type="range"]');
+
+const [
+  redColorInput,
+  greenColorInput,
+  blueColorInput,
+] = document.querySelectorAll('input[type="number"]');
+
+const updateColor = () => {
+  container.style.backgroundColor = `rgba(${redColorInput.value}, ${greenColorInput.value}, ${blueColorInput.value})`;
+};
+
+redColorSlider.oninput = ({ target: { value } }) => {
+  redColorInput.value = value;
+  updateColor();
+};
+
+greenColorSlider.oninput = ({ target: { value } }) => {
+  greenColorInput.value = value;
+  updateColor();
+};
+
+blueColorSlider.oninput = ({ target: { value } }) => {
+  blueColorInput.value = value;
+  updateColor();
+};
+
+updateColor();
